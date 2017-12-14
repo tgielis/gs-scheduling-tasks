7e2301351b300e57d00c1cb0ae5d1b38a60c2937
tgielis <gielistim@gmail.com>
2017-12-14 15:06:47 +0100

    test6

---
+++ target/dockerfile/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting License Server"
+echo "********************************************************"
+java -jar /usr/local/licensingservice/gs-scheduling-tasks-0.1.0.jar
